22d4fc9d5739e5df5da52ea7d941aa1bcb466f05
Merveille10 <merveille0sena@gmail.com>
2023-02-13 15:51:47 +0100

    Managing of questions's dispaying, Adding button to switch between questons

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,46 @@
 import {data} from "./data.js";
 // Wait for page to load
 window.addEventListener('load', ()=>{
-    // We will write all the content here
-    document.getElementById("question").innerHTML = firstelt['question'];
     // Variables
-    const firstelt = data[0];
+    const currentQuiz = document.querySelector("#current_quiz");
+    const question = document.querySelector("#question");
+    const answers = document.querySelectorAll("#btn_a");
+    const total_quiz = document.querySelector("#total_quiz");
+    const suivant = document.querySelector("#suivant");
+
+    let quizIndex = 0;
+    let quiz = data[quizIndex];
+    let suggestedAnswers = quiz.suggestedAnswer;
+    
+    
+    
+    // functions
+   
+   const showQuiz=(quiz)=>{
+        let suggestedAnswers = quiz.suggestedAnswer;
+        question.innerHTML = quiz.question;
+        currentQuiz.innerHTML = quiz.id;
+        suggestedAnswers.forEach(({answer, label, isRight}, index) => {
+            // answers[index].innerHTML = label + ": " + answer;
+            answers[index].innerHTML = `${label}: ${answer}` ;
+        });
+    }
+
+    //code
+    showQuiz(quiz);
+
+    suivant.addEventListener('click',()=> {
+        quizIndex++;
+        if (quizIndex == data.length-1) {
+            suivant.disabled = true;
+            suivant.style.cursor = "default";
+            suivant.style.opacity = 0.5;
+        }
+        quiz = data[quizIndex];
+        showQuiz(quiz);
+    }); 
+    
+    
     console.log(data.length);
-    const total_quiz = document.querySelector('#total_quiz');
-
     total_quiz.innerHTML = data.length;
-
-    //Functions
 });
